6fdca4e6f3e3be4f189a93845693fbb415507cc0
tlak <8529329+714k@users.noreply.github.com>
2024-02-10 03:00:33 -0600

    [FSRT] Removing unnecessary server files (#1)

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require("cors");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require("./routes/auth");
-const usersRoute = require("./routes/users");
-const moviesRoute = require("./routes/movies");
-const listsRoute = require("./routes/lists");
 const todos = require("./routes/todos");
 const db = require("./models");
-let corsOptions = {
+const corsOptions = {
   AccessControlAllowOrigin: '*',
   origin: "http://127.0.0.1:5173/",
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
@@ -38,14 +33,6 @@ app.use(bodyParser.json())
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("DB conection successfull"))
-.catch((error) => console.error(error));
-
-app.use("/api/auth", authRoute);
-app.use("/api/users", usersRoute);
-app.use("/api/movies", moviesRoute);
-app.use("/api/lists", listsRoute);
 app.use("/api/todos", todos);
 
 app.listen(PORT, () => {
